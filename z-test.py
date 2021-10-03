697077cb0a5a91237e8097872df744f6eccdd91d
TeodoraM <teodoramtc@gmail.com>
2021-10-03 12:44:05 +0200

    changed percentages to proportions

--- z-test.py
+++ z-test.py
@@ -3,17 +3,17 @@ import streamlit as st
 
 st.title("Two samples z-test calculator")
 
-p1 = st.number_input("Proportion in the first group:", min_value=float(0), max_value=float(100),
+p1 = st.number_input("Percentage in the first group (%):", min_value=float(0), max_value=float(100),
                      value=float(0), step=float(0.01))
-p2 = st.number_input("Proportion in the second group:", min_value=float(0), max_value=float(100),
+p2 = st.number_input("Percentage in the second group(%):", min_value=float(0), max_value=float(100),
                      value=float(0), step=float(0.01))
 n1 = st.number_input("Size of the first group:", min_value=float(2), max_value=float(10000000000), value=float(100),
                      step=float(1))
 n2 = st.number_input("Size of the second group:", min_value=float(2), max_value=float(10000000000), value=float(100),
                      step=float(1))
-p_pooled = (p1*n1 + p2*n2)/(n1+n2)
+p_pooled = (p1/100*n1 + p2/100*n2)/(n1+n2)
 
-z = round((p1 - p2) / np.sqrt((p_pooled*(1-p_pooled))*(1/n1 + 1/n2)), 2)
+z = round((p1/100 - p2/100) / np.sqrt((p_pooled*(1-p_pooled))*(1/n1 + 1/n2)), 2)
 
 
 ci = st.radio("Choose significance level: ", ('95%', '99%'))
